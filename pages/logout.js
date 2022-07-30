c1e1a193e685dec421a81ad595c2c4908d3b7974
Le Viet Hoang <hashdotlee@gmail.com>
2022-07-30 21:51:15 +0700

    fix: add router to logout

--- pages/logout.js
+++ pages/logout.js
@@ -1,3 +1,4 @@
+import {useRouter} from "next/router";
 import { useEffect } from "react";
 import { isLoggedIn, removeToken, removeUser } from "../utils/authUtils";
 export default function Logout() {
